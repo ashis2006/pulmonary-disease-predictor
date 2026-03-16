84d5db950aede3b21d885533409dbf37c2dc2857
ashis2006 <ashisjenisha.23ad@kct.ac.in>
2026-03-16 16:36:37 +0530

    audio processing issue

--- pulmonary_app/app.py
+++ pulmonary_app/app.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file, jsonify
 import sqlite3
 import os
